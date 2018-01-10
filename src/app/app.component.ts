e292280abf8d498a61fe028e2ccbbc7efae3d4d0
yitimo <admin@yitimo.com>
2018-01-10 09:44:16 +0800

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Yup } from 'yeui';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(
+    private yup: Yup
+  ) {}
+  public test() {
+    this.yup.alert({body: '你好啊'});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+import { YINModule, YupModule, yeuiThemeConfig } from 'yeui';
 
 import { AppComponent } from './app.component';
-
+// config the theme
+yeuiThemeConfig('weui');
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    YupModule
   ],
   providers: [],
   bootstrap: [AppComponent]
